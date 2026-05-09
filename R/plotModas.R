debc4bf5331e35a8eec4af113ea75144b0e2d055
Angel-PB <angelantonio.perez@rai.usc.es>
2026-05-09 18:34:59 +0200

    Ahora el método hace un plot nuevo si se le proporciona la muestra y dibuja sobre el antiguo en otro caso.

--- R/plotModas.R
+++ R/plotModas.R
@@ -1,8 +1,11 @@
 #' @exportS3Method
 
-plot.MRpms_modas <- function(x,...){
+plot.MRpms_modas <- function(x, data, ...){
+  if(!missing(data)){
+    plot(data)
+  }
   ni <- sapply(x, length)
-  xg <- rep(attr(x,"x.malla"), times = ni)
+  xg <- rep(attr(x, "x.malla"), times = ni)
   yg <- unlist(x)
-  graphics::points(xg,yg, ...)
+  graphics::points(xg, yg, ...)
 }
