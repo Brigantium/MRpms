0cdb885d61a7b4707779b240b0231de1da34e50f
Brigantium <ikerrod99@gmail.com>
2026-06-14 03:35:10 +0200

    Ahora tenemos páigna web. Para tener algo de verdad, incluí una primera versión de nuestro artículo introductorio a la regresión modal, con algunas modificaciones para que la introducción fuese más limpia. Siéntete libre de corregir lo que quieras (de hecho, hazlo). Para tocar este artículo ve al archivo "Modal_regresion.Rmd" que está en vignettes/articles. Es un Rmd, por lo que puedes hacer tanto como quieras. Para construir la web una vez hechos los cambios, ejecuta `build_site()`.  También cambié un poco la documentación del paquete de datos de los dos senos. Si tienes dudas, dime.

--- R/data.R
+++ R/data.R
@@ -1,8 +1,15 @@
-#' Ejemplo de los dos senos.
+#' @title Two sines shifted in Y axis.
 #'
-#' Dos senos a distintas alturas.
+#' The 200 points from two sines with a certain normal error,
+#' 
+#' * 100 from the usual sine ploted between 0 and 10 (in radians),
+#' 
+#' * 100 from a shifted sine 3 points lower between the same support. 
+#' 
+#' In both cases, error follows a certain \eqn{\mathcal N(0,0.3)}.
 #'
 #' @examples
 #' plot(twosines)
 #'
+#' @docType data
 "twosines"
